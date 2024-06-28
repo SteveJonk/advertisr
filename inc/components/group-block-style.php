1277e5843782e43da586de72627e312e60facd48
Steve Jonk <stevejonk1993@gmail.com>
2024-06-28 15:18:39 +0200

    feat: form grid

--- inc/components/group-block-style.php
+++ inc/components/group-block-style.php
@@ -39,3 +39,12 @@ register_block_style(
         'label'        => __('Background colors', 'textdomain'),
     )
 );
+
+
+register_block_style(
+    'core/group',
+    array(
+        'name'         => 'background-colors',
+        'label'        => __('Background colors', 'textdomain'),
+    )
+);
